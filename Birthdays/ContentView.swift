c5835169a736f9dbf30a3143882b9c160c149006
Chaylen <chaylennguyentran@gmail.com>
2025-07-25 10:32:14 -0700

    change the given list to a query to make code simpler and no existing data needed

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     
-    @State private var friends: [Friend] = [Friend(name: "Chaylen", birthday: .now), Friend(name: "Jeffrey", birthday: .now)]
+    @Query private var friends: [Friend]
+    @Environment(\.modelContext) private var context
     
     @State private var newName: String = ""
     @State private var newBirthday: Date = .now
@@ -17,7 +19,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationStack{
-            List(friends, id: \.name){ friend in
+            List(friends){ friend in
                 HStack{
                     Text(friend.name)
                     Spacer()
@@ -35,7 +37,7 @@ struct ContentView: View {
                     }
                     Button("Save"){
                         let newFriend = Friend(name: newName, birthday: newBirthday)
-                        friends.append(newFriend)
+                        context.insert(newFriend)
                         newName = ""
                         newBirthday = .now
                     }
@@ -54,4 +56,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+        .modelContainer(for: Friend.self, inMemory: true)
 }
