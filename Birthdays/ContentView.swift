2d314418bc5c988920f47ec155343a7f45bd2d31
Chaylen <chaylennguyentran@gmail.com>
2025-07-25 10:24:16 -0700

    added the save button to have the user input friend name and birthday and add it to the birthday list

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var friends: [Friend] = [Friend(name: "Chaylen", birthday: .now), Friend(name: "Jeffrey", birthday: .now)]
+    
+    @State private var newName: String = ""
+    @State private var newBirthday: Date = .now
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            List(friends, id: \.name){ friend in
+                HStack{
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom){
+                VStack(alignment: .center, spacing: 20){
+                    Text("Next Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now){
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save"){
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
+            
+            
         }
-        .padding()
+        
+        
     }
 }
 
